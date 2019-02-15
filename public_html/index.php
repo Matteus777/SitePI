8cfc3fabd9b177fb1a4becb69cf33f04e6dc5ef6
Matteus Camargo De Almeida <181710180@senacinformatica.br>
2019-02-15 15:00:15 -0200

    Adalto Pediu

---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LALALA</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div>TODO write content</div>
+        eieawdad
+    </body>
+</html>
