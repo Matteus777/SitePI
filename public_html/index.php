4f28da3a543124516ea48292165663d0822379e1
Matteus Camargo De Almeida <181710180@senacinformatica.br>
2019-02-15 16:43:40 -0200

    HTML E CSS DO MENU

--- public_html/index.php
+++ public_html/index.php
@@ -3,9 +3,18 @@
     <head>
         <title>LALALA</title>
         <meta charset="UTF-8">
+        <link href="estilo.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <div>TODO write content</div>
-        eieawdad
+        <div id="Menu">
+            <div id="Inicio">Inicio</div>
+            <div id="Quem">Quem Somos</div>
+            <div id="Agendar">Agendar</div>
+            <div id="Login">Login </div>
+            
+
+            
+            
+        </div>
     </body>
 </html>
