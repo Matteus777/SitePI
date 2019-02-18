1702f4429f9c8f9cc16292ee96a60a6dc0893cba
Matteus Camargo De Almeida <181710180@senacinformatica.br>
2019-02-18 15:00:19 -0300

    Arrumando as coisa aqui

--- public_html/index.php
+++ public_html/index.php
@@ -6,9 +6,37 @@
         <link href="estilo.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-            <div class="menu">Inicio</div>
-            <div class="menu">Quem Somos</div>
-            <div class="menu">Agendar</div>
-            <div class="menu">Login </div>
+            <a href="quemsomos.php">
+            <div id="Inicio">nbsp</div>
+            </a>
+            <div class="menu" id="Exames">Exames</div>
+            <div class="menu" id="Cirurgias">Cirurgias</div>
+            <div class="menu" id="Login">Login </div>
+    <!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+    <title>Menu Dropdown Horizontal - Linha de Código</title>
+     <!-- Aqui chamamos o nosso arquivo css externo -->
+    <link rel="stylesheet" type="text/css"  href="estilo.css" />
+    <!--[if lte IE 8]>
+ <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+ <![endif]-->   
+</head>
+<body>
+<nav>
+  <ul id="menuexames">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Sobre</a></li>
+            <li><a href="#">O que fazemos?</a>
+                <ul>
+                      <li><a href="#">Web Design</a></li>
+                      <li><a href="#">SEO</a></li>
+                      <li><a href="#">Design</a></li>                    
+                </ul>
+            </li>
+        <li><a href="#">Links</a></li>
+        <li><a href="#">Contato</a></li>                
+</ul>
     </body>
 </html>
