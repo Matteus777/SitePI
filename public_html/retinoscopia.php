57b5aaf83bc4763390959eb2fb6e02e4530b3250
Diogo Costa Goncalves <181710152@senacinformatica.br>
2019-02-19 17:00:26 -0300

    Criada paginas de exames

---
+++ public_html/retinoscopia.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Acuidade Visual - Clinica Bom Olhar</title>
+        <meta charset="UTF-8">        
+        <link href="estilo.css" rel="stylesheet" type="text/css"/>
+        <?php require_once ("/xampp/htdocs/t171/SitePI/public_html/index.php")?>
+        
+    </head>
+    <body>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div align="center">Exame de Acuidade Visual</div>
+        <div class="textos"> <p>Acuidade visual é a capacidade do olho para distinguir detalhes espaciais, ou seja, identificar o contorno e a forma dos objetos. A acuidade visual depende de fatores ópticos e neurais: da nitidez que a imagem chega na retina, da saúde das células retinianas e da capacidade de interpretação do cérebro.</p>
+        </div>
+        <div id="acuidade">&nbsp;</div>
+    </body>
+</html>
