fd6e3a0a027a363655dfb12c94f5d66aa8fa4118
Matteus Camargo De Almeida <181710180@senacinformatica.br>
2019-02-21 13:43:47 -0300

    pull

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,8 @@
         <title>Clínica Bom Olhar</title>
         <meta charset="UTF-8">
         <link href="estilo.css" rel="stylesheet" type="text/css"/>
-        
+        <script src="jquery3.3.1.js"></script>
+        <script src="Javascript.js"></script>
     </head>
     <body>
         <?php require_once 'menu.php'?>
