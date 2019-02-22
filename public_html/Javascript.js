59cd9bf1d401cf69bafb49be1a1881912b0ef8cd
matteus camargo <matteus.camargo@gmail.com>
2019-02-21 21:21:56 -0300

    ..

--- public_html/Javascript.js
+++
@@ -1,4 +0,0 @@
- $(".dropdown").hover(function dropdown(){
- $(".dropdown-content").css("background-color: red;");
- }
-);
---
+++ public_html/jquery.js
@@ -0,0 +1,6 @@
+ <script type="text/javascript" src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+ $(document).ready(function (){
+     $("div").click(function (){
+         $(this).hide();
+     });
+ });
