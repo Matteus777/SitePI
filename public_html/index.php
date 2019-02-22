350ea832d9676c621f14073ab3645cd0215fa893
Luan Alves Dos Santos <181710131@senacinformatica.br>
2019-02-22 14:20:27 -0300

    História da Clínica

--- public_html/index.php
+++ public_html/index.php
@@ -12,7 +12,20 @@
     </head>
     <body>
         <?php require_once 'menu.php'?>
-            <div id="fundo"  >&nbsp; </div>      
+            <div id="fundo"  >&nbsp; </div>    
+            <h2>História</h2>
+            
+            <p>Atuando no mercado desde 1990, fundada por Dr. Jorge e Dr. Geraldo, após se formarem, os filhos dos respectivos médicos, Dr. Vitor e Dr. Adalberto integraram a equipe. Ao longo desses anos, a Clínica Bom Olhar de Três Lagoas cresceu mantendo seu padrão de qualidade, ética e custo equilibrado na área da oftalmologia, agregando médicos oftalmologistas comprometidos com a atualização tecnológica para oferecer o melhor atendimento para a preservação da qualidade de vida de cada cliente que procura os nossos cuidados.
+Temos como missão: Zelar pelo bem-estar do cliente, garantindo um melhor desempenho para a saúde ocular. Diante desse objetivo, agregamos os seguintes valores à Clínica Bom Olhar:
+</p>
+<ul>
+    <li>Comprometimento;</li>
+    <li>Responsabilidade;</li>
+    <li>Ética;</li>
+</ul>
+            
+            
+                        
     </body>
     </html>
 
