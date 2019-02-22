6cee8dce6bc09f479a0701a09104e6e17a0bfd3b
Matteus Camargo De Almeida <181710180@senacinformatica.br>
2019-02-22 14:30:55 -0300

    push

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <link href="estilo.css" rel="stylesheet" type="text/css"/>
        
-<!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
         
         <script src="jquery3_3_1.js"></script>
         <script src="Javascript.js"></script>
