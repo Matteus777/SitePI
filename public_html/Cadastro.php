2941ad44be40fee478913c87fa3bf365d9bf4cf8
matteus camargo <matteus.camargo@gmail.com>
2019-02-26 21:26:30 -0300

    terminei o login MERDA q o diogo fez

--- public_html/Cadastro.php
+++ public_html/Cadastro.php
@@ -6,10 +6,8 @@
          
   </head>
 <body>
-
-    
-      
-         Formulário
+<?php require_once 'menu.php'?>
+    <h2>Preencha os campos para realizar o cadastro.</h2>
          <div class="cadastros">Nome:<input  type="text"></div>         
          <div class="cadastros">Email:<input type="text"></div>         
          <div class="cadastros">CPF:<input type="text"></div>         
@@ -23,5 +21,6 @@
          </div>
          <div class="cadastros" >Idade:<input type="number" min="0" max="100"></div>
          <div class="cadastros" >senha:<input type="password"></div>
-    </body>
+    <?php require_once 'rodape.php'?>
+</body>
 </html>
